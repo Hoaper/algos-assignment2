f7c4a9dd547a59f344e383a6c4897f6b915fe989
zh0per <beeboplay@gmail.com>
2023-04-10 19:03:25 +0600

    add function doc added

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -19,7 +19,11 @@ public class MyLinkedList<E> implements MyList{
     MyLinkedList() {
         size = 0;
     }
-
+    /**
+     * @function size возвращает длину LinkedList
+     * @noparams
+     * @return int
+     * **/
     @Override
     public int size() {
         return this.size;
@@ -46,10 +50,32 @@ public class MyLinkedList<E> implements MyList{
         this.tail = node;
         size++;
     }
-
+    /**
+     * @function add добавляет объект на определенный индекс
+     * @param item объект для добавления
+     * @param index индекс куда нужно добавить item
+     * @return void
+     * **/
     @Override
     public void add(Object item, int index) {
-
+        Node oldNode = getNodeByIndex(index);
+        Node node = new Node<E>((E) item);
+        if (this.head.equals(oldNode)) {
+            node.next = this.head;
+            this.head.previous = node;
+            this.head = node;
+        } else if (this.tail.equals(oldNode)){
+            node.next = this.tail;
+            node.previous = this.tail.previous;
+            this.tail.previous.next = node;
+            this.tail.previous = node;
+        } else {
+            node.previous = oldNode.previous;
+            node.next = oldNode;
+            oldNode.previous.next = node;
+            oldNode.previous = node;
+        }
+        size++;
     }
 
     @Override
@@ -64,7 +90,7 @@ public class MyLinkedList<E> implements MyList{
     @Override
     public Object remove(int index) {
         checkIndex(index);
-        Node node = findIndexedNode(index);
+        Node node = getNodeByIndex(index);
 
         if (node.previous == null) {
             this.head = node.next;
@@ -102,7 +128,7 @@ public class MyLinkedList<E> implements MyList{
     @Override
     public Object get(int index) {
         checkIndex(index);
-        return findIndexedNode(index).val;
+        return getNodeByIndex(index).val;
     }
 
     @Override
@@ -119,7 +145,7 @@ public class MyLinkedList<E> implements MyList{
     public void sort() {
 
     }
-    private Node findIndexedNode(int index) {
+    private Node getNodeByIndex(int index) {
         checkIndex(index);
         Node toSearch;
         toSearch = this.head;
