fb3c40a00c6dd8c6e2872d4c39fdbd45ae8f8903
zh0per <beeboplay@gmail.com>
2023-04-29 17:20:34 +0600

    MyArrayListQueue.java completed

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,35 +1,30 @@
-//public class MyArrayListQueue {
-//    MyLinkedList myLinkedList = new <T>MyLinkedList();
-//
-//    MyLinkedListQueue() {}
-//
-//    public void enqueue(T element) {
-//        myLinkedList.add(element);
-//    }
-//    public T dequeue() {
-//        return (T) myLinkedList.remove(0);
-//    }
-//
-//    public T pop() throws NoSuchFieldException {
-//        isEmptyThrowException();
-//        return (T) myLinkedList.remove(0);
-//    }
-//
-//    public T peek() throws NoSuchFieldException {
-//        isEmptyThrowException();
-//        return (T) myLinkedList.get(0);
-//    }
-//
-//    public int size() {
-//        return myLinkedList.size();
-//    }
-//    private void isEmptyThrowException() throws NoSuchFieldException {
-//        if (isEmpty()) {
-//            throw new NoSuchFieldException();
-//        }
-//    }
-//
-//    public boolean isEmpty() {
-//        return this.size() == 0;
-//    }
-//}
+public class MyArrayListQueue<T> {
+    MyArrayList myArrayList = new <T>MyArrayList();
+
+    MyArrayListQueue() {}
+
+    public void enqueue(T element) {
+        myArrayList.add(element);
+    }
+    public T dequeue() {
+        return (T) myArrayList.remove(0);
+    }
+
+    public T peek() throws NoSuchFieldException {
+        isEmptyThrowException();
+        return (T) myArrayList.get(0);
+    }
+
+    public int size() {
+        return myArrayList.size();
+    }
+    private void isEmptyThrowException() throws NoSuchFieldException {
+        if (isEmpty()) {
+            throw new NoSuchFieldException();
+        }
+    }
+
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+}
