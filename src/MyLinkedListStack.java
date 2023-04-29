32b2964700de8888492dbcdb697374baf698dd7a
zh0per <beeboplay@gmail.com>
2023-04-29 16:41:33 +0600

    isEmpty Added

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -5,19 +5,31 @@ import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
 
     MyLinkedList myLinkedList = new <T>MyLinkedList();
-    MyLinkedListStack() {}
-    public void push(T element){
+
+    MyLinkedListStack() {
+    }
+
+    public void push(T element) {
         myLinkedList.add(element, 0);
     }
+
     public T pop() {
-        isEmpty();
+        isEmptyThrowException();
         return (T) myLinkedList.remove(0);
     }
+
     public T peek() {
-        isEmpty();
+        isEmptyThrowException();
         return (T) myLinkedList.get(0);
     }
-    private void isEmpty() {
-        if (myLinkedList.size() == 0) throw new EmptyStackException();
+
+    private void isEmptyThrowException() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+    }
+
+    public boolean isEmpty() {
+        return myLinkedList.size() == 0;
     }
 }
