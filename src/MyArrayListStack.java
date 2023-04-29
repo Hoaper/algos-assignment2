b3acf08d623cab94cba061c18673c81b509c5e57
zh0per <beeboplay@gmail.com>
2023-04-29 17:12:41 +0600

    MyArrayListStack.java added

---
+++ src/MyArrayListStack.java
@@ -0,0 +1,34 @@
+import java.util.EmptyStackException;
+
+public class MyArrayListStack<T> {
+    MyArrayList myArrayList = new <T>MyArrayList();
+
+    MyArrayListStack() {}
+
+    public void push(T element) {
+        myArrayList.add(element, 0);
+    }
+
+    public T pop() {
+        isEmptyThrowException();
+        return (T) myArrayList.remove(0);
+    }
+
+    public T peek() {
+        isEmptyThrowException();
+        return (T) myArrayList.get(0);
+    }
+
+    public int size() {
+        return myArrayList.size();
+    }
+    private void isEmptyThrowException() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+    }
+
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+}
